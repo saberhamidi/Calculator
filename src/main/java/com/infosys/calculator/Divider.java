dc7d39d3d546a8170d697cf82805812c54016f77
Saber Hamidi <sh30g12@soton.ac.uk>
2018-04-19 10:46:16 +0100

    Divider feature refactored

--- src/main/java/com/infosys/calculator/Divider.java
+++ src/main/java/com/infosys/calculator/Divider.java
@@ -3,8 +3,7 @@ package com.infosys.calculator;
 public class Divider {
 
     public int divide(int num1, int num2) {
-        int result = num1/num2;
 
-        return result;
+        return num1/num2;
     }
 }
--- src/test/java/com/infosys/calculator/DividerTest.java
+++ src/test/java/com/infosys/calculator/DividerTest.java
@@ -8,8 +8,6 @@ public class DividerTest {
     @org.junit.Test
     public void Test(){
         divider = new Divider();
-        int expected = 3;
-        int actual = divider.divide(15,5);
-        assertEquals(expected, actual);
+        assertEquals(3, divider.divide(15,5));
     }
 }
